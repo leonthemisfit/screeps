56a7e8f1cabd7a6ec045519ad9bdcf95366acb98
leonthemisfit <leonthemisfit@gmail.com>
2018-06-23 15:54:51 -0400

    Refactored main

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ var roles = {
 }
 
 const WORKER_LIMIT = 2;
+const DEFAULT_ROLE = "harvester";
+const WORKER_NAME = "worker_";
+
 var worker_id = 20;
 
 var worker_roles = ["harvester", "upgrader", "builder"];
@@ -15,23 +18,28 @@ var worker_filter = (creep) => worker_roles.includes(creep.memory.role);
 
 var worker_spawns = [creeps.hard_worker, creeps.better_worker, creeps.basic_worker];
 
-module.exports.loop = () => {
-    util.clean_memory();
+function autospawn() {
+    var workers = _.filter(Game.creeps, worker_filter);
+    var spawner = Game.spawns.Main;
+    if (workers.length < WORKER_LIMIT) {
+        for (var i in worker_spawns) {
+            var parts = worker_spawns[i];
+            if (util.can_spawn(spawner, parts)) {
+                var name = WORKER_NAME + worker_id++;
+                util.try_spawn(spawner, name, parts, DEFAULT_ROLE);
+            }
+        }
+    }
+}
 
+function role_check() {
     var workers = _.filter(Game.creeps, worker_filter);
     if (util.is_spawner_full(Game.spawns.Main)) {
         for (var i in workers) {
             var creep = workers[i];
             creep.memory.role = util.is_construction(creep) ? "builder" : "upgrader";
         }
-        if (workers.length < WORKER_LIMIT) {
-            for (var i in worker_spawns) {
-                var parts = worker_spawns[i];
-                if (util.can_spawn(Game.spawns.Main, parts)) {
-                    util.try_spawn(Game.spawns.Main, "worker_" + worker_id++, parts, "harvester");
-                }
-            }
-        }
+        autospawn();
     }
     else {
         for (var i in workers) {
@@ -39,10 +47,18 @@ module.exports.loop = () => {
             creep.memory.role = "harvester";
         }
     }
+}
 
+function do_roles() {
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         var role = roles[creep.memory.role];
         role(creep);
     }
+}
+
+module.exports.loop = () => {
+    util.clean_memory();
+    role_check();
+    do_roles();
 }
