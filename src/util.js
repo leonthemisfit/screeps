8ef84dab7a0c600f5b1dfed562c713e53d9b7c93
leonthemisfit <leonthemisfit@gmail.com>
2018-06-23 13:27:35 -0400

    Fixed spawn util function

--- src/util.js
+++ src/util.js
@@ -1,5 +1,3 @@
-var creep_types = require("creeps");
-
 var util = {};
 
 util.find_source = (creep) => creep.room.find(FIND_SOURCES)[0];
@@ -10,7 +8,7 @@ util.try_harvest = (creep, source) => creep.harvest(source) != ERR_NOT_IN_RANGE;
 
 util.try_transfer_energy = (creep, target) => creep.transfer(target, RESOURCE_ENERGY) != ERR_NOT_IN_RANGE;
 
-util.try_spawn = (spawner, name, type, role) => spawner.spawnCreep(creep_types[type], name, { memory: { role: role } })
+util.try_spawn = (spawner, name, creep_type, role) => spawner.spawnCreep(creep_type, name, { memory: { role: role } })
 
 util.try_upgrade = (creep, controller) => creep.upgradeController(controller) != ERR_NOT_IN_RANGE;
 
