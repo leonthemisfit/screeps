9faa9e8b7588ff9527dba1d0869ea7b54a03ccda
leonthemisfit <leonthemisfit@gmail.com>
2018-06-23 14:08:02 -0400

    Added build cost calculator

---
+++ src/build_cost.js
@@ -0,0 +1,26 @@
+var parts = {};
+
+parts[MOVE] = 50;
+parts[WORK] = 100;
+parts[CARRY] = 50;
+parts[ATTACK] = 80;
+parts[RANGED_ATTACK] = 150;
+parts[HEAL] = 250;
+parts[CLAIM] = 600;
+parts[TOUGH] = 10;
+
+function calc(body) {
+    var total = 0;
+    for (var i in body) {
+        var part = body[i];
+        if (parts[part]) {
+            total += parts[part];
+        }
+    }
+    return total;
+}
+
+module.exports = {
+    part_costs: parts,
+    calculate_cost: calc
+};
