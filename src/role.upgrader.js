4cd6e1e588bdf5682620632a8fa67df5db773025
Leon <leonthemisfit@gmail.com>
2018-06-23 22:31:49 -0400

    Updated upgrader role to use flags to prevent collisions

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -5,6 +5,8 @@ var modes = {
     upgrading: 1
 }
 
+var flags = ["upgrade_1", "upgrade_2", "upgrade_3"]
+
 function harvest(creep) {
     var source = util.find_source(creep);
     if (!util.try_harvest(creep, source)) {
@@ -25,7 +27,30 @@ function run(creep) {
     }
     else if (creep.memory.upgrade_mode == modes.upgrading) {
         if (!util.try_upgrade(creep, creep.room.controller)) {
-            creep.moveTo(creep.room.controller);
+            if (creep.memory.moving) {
+                return;
+            }
+            for (var id in flags) {
+                var flag = Game.flags[id]
+                if (flag.memory.available === undefined) {
+                    flag.memory.available = true;
+                }
+                if (flag.memory.available) {
+                    flag.memory.available = false;
+                    creep.memory.moving = true;
+                    creep.memory.flag_id = id;
+                    creep.moveTo(flag);
+                    break;
+                }
+            }
+        }
+        else {
+            creep.memory.moving = false;
+            if (creep.carry.energy == 0) {
+                var flag = Game.flags[creep.memory.flag_id];
+                flag.memory.available = true;
+                delete creep.memory.flag_id;
+            }
         }
     }
     else {
